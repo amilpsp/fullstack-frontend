1718a2ffca146c3ef76a24371fda2648aaa6064e
celoob <carlemilloob@gmail.com>
2024-05-19 13:21:23 +0200

    Added some pages and components, all empty though.

---
+++ src/Components/Navbar/Navbar.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import './Navbar.css'
+
+/* interface Props {} */
+
+const Navbar = (/* props: Props */) => {
+    return <div>Navbar</div>
+}
+
+export default Navbar
---
+++ src/Components/Thread/Thread.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import './Thread.css'
+
+/* interface Props {} */
+
+const Thread = (/* props: Props */) => {
+    return (
+        <div className="thread">
+            <p>Thread</p>
+        </div>
+    )
+}
+
+export default Thread
