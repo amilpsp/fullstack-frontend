0b888b2c5cffbb1d2a2ce9f67c80055018d473c4
Amanda Ruiz <98482837+amilpsp@users.noreply.github.com>
2024-05-20 18:58:13 +0200

    Structure ready

--- src/Pages/ThreadPage/ThreadPage.tsx
+++ src/Pages/ThreadPage/ThreadPage.tsx
@@ -1,27 +1,36 @@
 import './ThreadPage.css'
+import Reply from "../../Components/Reply/Reply";
+import "/src/Components/Reply/Reply.tsx";
 /* import { TextField } from "@mui/material"; /* type Props = {} */
 
 const ThreadPage = (/* props: Props */) => {
   return (
-    <div id="page" className='flex flex-col gap-6'>
-        <div className='jusify-self-center'>BREADCRUMS GO HERE</div>
-      <section className="align-stretch w-[50vw]">
-        <article id="post-body" className="border rounded-lg p-2 ">
-          <div className="flex flex-row justify-between gap-2">
-            <h2 className="text-xl">Thread title</h2>{" "}
-            <div className='text-xs'>
+    <div
+      id="thread-page"
+      className="flex flex-col align-stretch w-[50vw] gap-6"
+    >
+      <div>BREADCRUMS GO HERE</div>
+      <section className="flex flex-col gap-2 ">
+        <article id="post-body" className="border rounded-lg p-4">
+          <div className="flex flex-row justify-between">
+            <h2 className="text-xl">Thread title</h2>
+            <div className="text-xs">
               posted on <span>forum</span>
             </div>
           </div>
           <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi
-            asperiores porro magnam eligendi, quod ea expedita fugit delectus
-            aliquid alias enim consequuntur. Quibusdam, a nisi! Reprehenderit
-            totam atque maxime obcaecati ex eligendi accusantium rerum facilis,
-            nam quis ipsum deserunt? Fugit omnis quisquam sequi earum veniam,
-            deserunt officiis quasi impedit, ipsa commodi praesentium assumenda
-            obcaecati recusandae? Officia reiciendis voluptates explicabo
-            assumenda?
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus eum
+            sint ullam tempora fugit aut, non eos nisi laborum porro eveniet
+            blanditiis dicta quaerat ipsa cumque deserunt? Dolore earum possimus
+            laboriosam fuga similique perferendis suscipit quis aliquid saepe,
+            aut voluptatum commodi debitis officiis minima aperiam quibusdam
+            amet aspernatur recusandae blanditiis. Fuga, ab aliquid. Animi
+            dignissimos veritatis labore libero cum molestiae accusamus sunt
+            voluptate voluptas, reprehenderit voluptatum ex sit quo at porro,
+            perspiciatis architecto eligendi aspernatur repudiandae tempora
+            ipsam repellat repellendus ad? Tenetur animi nisi cum ducimus illo
+            porro blanditiis libero, a nemo quae eveniet labore doloribus,
+            reprehenderit repudiandae iste beatae.
           </p>
         </article>
         {/* end of post body */}
@@ -30,17 +39,23 @@ const ThreadPage = (/* props: Props */) => {
           <span>created</span>
         </div>
       </section>
-      
+
       <input
         type="text"
-        aria-label="Add-reply"
+        aria-label="add-reply"
         placeholder="Add a reply"
-        className='text-white bg-black border rounded-lg p-2'
+        className="text-white bg-black border rounded-lg p-2"
       ></input>
 
-      <div className=''><span>3 replies</span> <hr/></div>
+      <section className="flex flex-col gap-6">
+        <div>
+          <span className="text-xl">3 replies</span> <hr className="mb-4" />
+        </div>
+        <Reply />
+        <Reply />
+        <Reply />
+      </section>
     </div>
-
   );
 };
 
