e20347c1cf6270fbe11dce9ab5b8b17c1372edec
celoob <carlemilloob@gmail.com>
2024-05-20 15:44:33 +0200

    Footer component and outlet css

--- src/App.tsx
+++ src/App.tsx
@@ -8,8 +8,10 @@ function App() {
         <>
             <div className="bg-bgcol text-txtbright">
                 <Navbar />
-                <div className="p-5 text-sm">
-                    <Outlet />
+                <div className="flex justify-center p-5 text-sm min-h-180">
+                    <div className="max-w-[1080px]">
+                        <Outlet />
+                    </div>
                 </div>
                 <Footer />
             </div>
--- src/Components/Navbar/Navbar.tsx
+++ src/Components/Navbar/Navbar.tsx
@@ -7,7 +7,7 @@ const Navbar: React.FC = () => {
         <nav className="relative flex w-full flex-wrap items-center justify-between bg-bgcol border-b-[1px] border-bordercol h-24">
             <div className="flex w-full flex-wrap items-center justify-between px-3">
                 {/* LEFT - Logo */}
-                <div className="flex">
+                <div className="flex ml-16">
                     <Link
                         to=""
                         className="mx-2 my-1 flex items-center lg:mb-0 lg:mt-0"
@@ -20,15 +20,19 @@ const Navbar: React.FC = () => {
                     </Link>
                 </div>
                 {/* MIDDLE - Search Bar*/}
-                <div className="mx-auto hidden flex-row ps-0 md:flex">
+                <div className="mx-auto flex-row ps-0 md:flex">
                     <SearchBar />
                 </div>
 
                 {/* RIGHT - Create, My Threads, Login */}
-                <ul className="list-style-none flex flex-row ps-0 md:ps-4">
-                    <li className="px-2">icon 1</li>
-                    <li className="px-2">icon 2</li>
-                    <li className="px-2">icon 3</li>
+                <ul className="flex flex-row mr-16">
+                    <li className="px-2">
+                        <Link to="/">Create</Link>
+                    </li>
+                    <li className="px-2">
+                        <Link to="/account">My threads</Link>
+                    </li>
+                    <li className="px-2">Login</li>
                 </ul>
             </div>
         </nav>
