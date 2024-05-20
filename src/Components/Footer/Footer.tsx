3ba7b2c49dd133497411632a5cdc15113b853a05
celoob <carlemilloob@gmail.com>
2024-05-20 15:46:50 +0200

    footer tweak

--- src/Components/Footer/Footer.tsx
+++ src/Components/Footer/Footer.tsx
@@ -10,7 +10,7 @@ const Footer = () => {
                 <li className="px-2">YT</li>
                 <li className="px-2">FB</li>
             </ul>
-            <p className="text-txtdark text-sm">
+            <p className="text-txtdark text-sm mb-14">
                 The content of this site is copyright-protected and is the
                 property of Fully Stacked Team, ITHS.
             </p>
