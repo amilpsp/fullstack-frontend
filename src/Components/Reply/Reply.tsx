0efa88b65ce05bc738aaa847ee9078a92e75102b
Amanda Ruiz <98482837+amilpsp@users.noreply.github.com>
2024-05-21 12:04:40 +0200

    Props ready and changed to accomodate for separation of date/time

--- src/Components/Reply/Reply.tsx
+++ src/Components/Reply/Reply.tsx
@@ -1,20 +1,23 @@
-const Reply = () => {
-  return (
-    <>
-      <article>
-        <p className="border rounded-lg p-4">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Quia hic
-          aspernatur repellat sint, dicta amet autem quo ipsum reprehenderit
-          enim?
-        </p>
-        <div className="flex justify-end mt-2 text-xs ">
-          <div className="flex gap-4">
-            <span>author </span>
-            <span>created</span>
-          </div>
-        </div>
-      </article>
-    </>
-  );
+type Props = {
+	replyBody: string;
+	replyAuthor: string;
+	replyCreatedDate: string;
+	replyCreatedTime: string;
+};
+const Reply = (props: Props) => {
+	return (
+		<>
+			<article>
+				<p className="border rounded-lg p-4">{props.replyBody}</p>
+				<div className="flex justify-end mt-2 text-xs ">
+					<div className="flex gap-4">
+						<span>{props.replyAuthor} </span>
+						<span>{props.replyCreatedDate}</span>
+						<span>{props.replyCreatedTime}</span>
+					</div>
+				</div>
+			</article>
+		</>
+	);
 };
 export default Reply;
--- src/Pages/ThreadPage/ThreadPage.tsx
+++ src/Pages/ThreadPage/ThreadPage.tsx
@@ -1,59 +1,60 @@
 import './ThreadPage.css'
 import Reply from "../../Components/Reply/Reply";
 import "/src/Components/Reply/Reply.tsx";
-/* import { TextField } from "@mui/material"; /* type Props = {} */
-
-const ThreadPage = (/* props: Props */) => {
+/* import { TextField } from "@mui/material";  */
+type Props = {
+  threadTitle:string;
+  forum:string;
+  postContent:string;
+  author:string;
+  createdDate:string;
+  createdTime:string;
+  replyAmount:number;
+}
+const ThreadPage = (props: Props) => {
   return (
     <div
       id="thread-page"
       className="flex flex-col align-stretch w-[50vw] gap-6"
     >
       <div>BREADCRUMS GO HERE</div>
-      <section className="flex flex-col gap-2 ">
+      <section className="flex flex-col gap-6 ">
         <article id="post-body" className="border rounded-lg p-4">
           <div className="flex flex-row justify-between">
-            <h2 className="text-xl">Thread title</h2>
+            <h2 className="text-xl">{props.threadTitle}</h2>
             <div className="text-xs">
-              posted on <span>forum</span>
+              posted on <span>{props.forum}</span>
             </div>
           </div>
           <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus eum
-            sint ullam tempora fugit aut, non eos nisi laborum porro eveniet
-            blanditiis dicta quaerat ipsa cumque deserunt? Dolore earum possimus
-            laboriosam fuga similique perferendis suscipit quis aliquid saepe,
-            aut voluptatum commodi debitis officiis minima aperiam quibusdam
-            amet aspernatur recusandae blanditiis. Fuga, ab aliquid. Animi
-            dignissimos veritatis labore libero cum molestiae accusamus sunt
-            voluptate voluptas, reprehenderit voluptatum ex sit quo at porro,
-            perspiciatis architecto eligendi aspernatur repudiandae tempora
-            ipsam repellat repellendus ad? Tenetur animi nisi cum ducimus illo
-            porro blanditiis libero, a nemo quae eveniet labore doloribus,
-            reprehenderit repudiandae iste beatae.
+            {props.postContent}
           </p>
         </article>
         {/* end of post body */}
         <div id="post-info" className="flex flex-row justify-end gap-2">
-          <span>author</span>
-          <span>created</span>
+          <span>{props.author}</span>
+          <span>{props.createdDate}</span>
+          <span>{props.createdTime}</span>
         </div>
       </section>
 
-      <input
-        type="text"
-        aria-label="add-reply"
-        placeholder="Add a reply"
-        className="text-white bg-black border rounded-lg p-2"
-      ></input>
+      <form action="" className='flex gap-2'>
+        <textarea
+          id='add-reply'
+          name="add-reply"
+          placeholder="Add a reply"
+          className="flex-grow text-white bg-black border rounded-lg p-2"
+          ></textarea>
+        <input type="submit" value='submit' className='border rounded-xl w-fit px-2 py-1 grow-0' />
+      </form>
 
-      <section className="flex flex-col gap-6">
+      <section className="flex flex-col gap-4">
         <div>
-          <span className="text-xl">3 replies</span> <hr className="mb-4" />
+          <span className="text-xl">{props.replyAmount} replies</span> <hr className="mb-4" />
         </div>
-        <Reply />
-        <Reply />
-        <Reply />
+        <Reply replyBody='Hello there' replyAuthor='Kenobi' replyCreatedDate='today'replyCreatedTime='01:23' />
+        <Reply replyBody='General Kenobiiii' replyAuthor='Grievous' replyCreatedDate='today'replyCreatedTime='01:23' />
+        
       </section>
     </div>
   );
