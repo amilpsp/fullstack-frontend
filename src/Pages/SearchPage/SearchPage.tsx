d204b8e0c6f8affdd24ecb5c5edb3a3b9f9b3fcb
celoob <carlemilloob@gmail.com>
2024-05-21 12:15:46 +0200

    SearchPage+SmTopicButton component

--- src/Pages/SearchPage/SearchPage.tsx
+++ src/Pages/SearchPage/SearchPage.tsx
@@ -1,13 +1,20 @@
-import './SearchPage.css'
+import SmTopicButtons from '../../Components/SmTopicButtons/SmTopicButtons';
+import './SearchPage.css';
 
-/* type Props = {} */
+const SearchPage = () => {
+  return (
+    <div className="text-center w-[34vw] text-txtdark">
+      {/* Breadcrumbs here */}
+      <p className="m-1">/Search</p>
+      <p className="m-4">Search specific topics:</p>
+      <SmTopicButtons />
+      <p className="m-4">
+        Found X threads containing{' '}
+        <span className="text-txtbright">"Placeholder"</span> in games
+      </p>
+      {/* THREAD TABLE GOES HERE */}
+    </div>
+  );
+};
 
-const SearchPage = (/* props: Props */) => {
-    return (
-        <div>
-            <p>This is path "/search", Searchpage content goes here</p>
-        </div>
-    )
-}
-
-export default SearchPage
+export default SearchPage;
