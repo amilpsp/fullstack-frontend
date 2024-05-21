3616753ded13d3249ada361159b137a00cc97452
Andrei Pirogov Huertas <andrei.pirogov.huertas@gmail.com>
2024-05-21 14:54:12 +0200

    Added model for posts, postsContainer component created, homepage structure setup

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,1 +1,16 @@
 /// <reference types="vite/client" />
+
+interface Post {
+  name: string;
+  content: string;
+  date: string;
+  time: string;
+  originalPoster: string;
+  topic: string;
+  replies: number;
+  lastReply: {
+    name: string;
+    date: string;
+    time: string;
+  };
+}
