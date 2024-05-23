ad43d46f392fb5b543f6afb0210361e43b626f8e
Amanda Ruiz <98482837+amilpsp@users.noreply.github.com>
2024-05-23 16:13:38 +0200

    interfaces for Comment, ThreadPage, NewThreadPage, and AccountPage

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,37 +1,51 @@
 /// <reference types="vite/client" />
-
 interface Post {
-  id: number;
-  name: string;
-  content: string;
-  date: string;
-  time: string;
-  originalPoster: string;
-  topic: string;
-  replies: number;
-  lastReply: {
-    name: string;
-    date: string;
-    time: string;
-  };
+	id: number;
+	name: string;
+	content: string;
+	date: string;
+	time: string;
+	originalPoster: string;
+	topic: string;
+	replies: number;
+	lastReply: {
+		name: string;
+		date: string;
+		time: string;
+	};
+}
+
+interface Comment {
+	id: number;
+	content: string;
+	date: string;
+	time: string;
+	commenter: string;
 }
 
 interface ThreadPage {
-	post: {
-		id: number;
-		name: string;
-		content: string;
-		date: string;
-		time: string;
-		originalPoster: string;
-		forum: string;
-		replyAmount: number;
-	};
-	comments: {
-		id: number;
-		content: string;
-		date: string;
-		time: string;
-		commenter: string;
-	};
+	id: number;
+	name: string;
+	content: string;
+	date: string;
+	time: string;
+	originalPoster: string;
+	topic: string;
+	replies: number;
+	comments: Comment[];
+}
+
+interface NewThreadPage {
+	name: string;
+	content: string;
+	originalPoster: string;
+	topic: string;
+	replies: number;
+	comments: Comment[];
+}
+
+interface AccountPage {
+	id: string;
+	name: string;
+	recentThreads: Post[];
 }
