eda2891dba217d5183c9e9ea60da8540c73be7dc
Amanda Ruiz <98482837+amilpsp@users.noreply.github.com>
2024-05-29 17:09:38 +0200

    edited User global interface and fixed its instances

--- src/Components/SignupLogin/SignupLogin.tsx
+++ src/Components/SignupLogin/SignupLogin.tsx
@@ -62,7 +62,7 @@ const SignupLogin: React.FC = () => {
 		axios(loginConfig)
 			.then((result) => {
 				localStorage.setItem("userLogged", JSON.stringify(result.data));
-				login({ name: username });
+				login(result.data);
 				console.log(result.data);
 			})
 			.catch((error) => {
@@ -84,7 +84,7 @@ const SignupLogin: React.FC = () => {
 		axios(signupConfig)
 			.then((result) => {
 				localStorage.setItem("userLogged", JSON.stringify(result.data));
-				login({ name: newUsername });
+				login({ username: newUsername });
 				console.log(result.data);
 			})
 			.catch((error) => {
@@ -132,7 +132,7 @@ const SignupLogin: React.FC = () => {
 										d="M10 7.5a2.5 2.5 0 1 1-5 0 2.5 2.5 0 0 1 5 0Zm0 0 .211-.106a4 4 0 0 1 3.578 0L14 7.5m0 0a2.5 2.5 0 1 0 5 0 2.5 2.5 0 0 0-5 0Zm-2 6.303c5-3 5 3.5 9 1.767-1 4.233-6 4.233-9 1.233-3 3-8 3-9-1.233 4 1.733 4-4.767 9-1.767Z"
 									/>
 								</svg>
-								<p>{user.name}</p>
+								<p>{user.username}</p>
 							</button>
 						</MenuButton>
 						<Menu
