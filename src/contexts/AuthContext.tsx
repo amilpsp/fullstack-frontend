a0b8cad9e61b8fa815fbff0ae083853c8bf3d300
Amanda Ruiz <98482837+amilpsp@users.noreply.github.com>
2024-05-30 17:16:01 +0200

    prettier is effing with this, but essentially i moved the axios calls to the DB into the AuthContext files, changing the login function and adding a signup one.

--- src/contexts/AuthContext.tsx
+++ src/contexts/AuthContext.tsx
@@ -1,38 +1,66 @@
+import axios from "axios";
 import React, { createContext, useState, ReactNode } from "react";
 
 // Define types for user and context
 
 export interface AuthContextType {
-  user: User | null;
-  login: (userData: User) => void;
-  logout: () => void;
+	user: User | null;
+	signup: (userData: User) => void;
+	login: (userData: User) => void;
+	logout: () => void;
 }
 
 // Create the context
 export const AuthContext = createContext<AuthContextType | undefined>(
-  undefined
+	undefined
 );
 
 interface AuthProviderProps {
-  children: ReactNode;
+	children: ReactNode;
 }
 
 // Create a provider component
 export const AuthProvider: React.FC<AuthProviderProps> = ({ children }) => {
-  const [user, setUser] = useState<User | null>(null);
+	const [user, setUser] = useState<User | null>(null);
 
-  const login = (userData: User) => {
-    console.log("Logging in user:", userData); // Log user data
-    setUser(userData);
-  };
+	const signup = async (userData: User) => {
+		const signupConfig = {
+			method: "post",
+			url: "http://localhost:8080/signup",
+			data: userData,
+		};
+		try {
+			const result = await axios(signupConfig);
+			login(result.data);
+		} catch (error) {
+			console.log("error in AuthContext signup function");
+		}
+	};
 
-  const logout = () => {
-    setUser(null);
-  };
+	const login = async (userData: User) => {
+		const loginConfig = {
+			method: "post",
+			url: "http://localhost:8080/login",
+			data: userData,
+		};
+		try {
+			const result = await axios(loginConfig);
+			localStorage.setItem("userLogged", JSON.stringify(result.data));
+			//login(result.data);
+		} catch (error) {
+			console.log("error in AuthContext login function");
+		}
+		console.log("Logging in user:", userData.username); // Log user data
+		setUser(userData);
+	};
 
-  return (
-    <AuthContext.Provider value={{ user, login, logout }}>
-      {children}
-    </AuthContext.Provider>
-  );
+	const logout = () => {
+		setUser(null);
+	};
+
+	return (
+		<AuthContext.Provider value={{ user, signup, login, logout }}>
+			{children}
+		</AuthContext.Provider>
+	);
 };
