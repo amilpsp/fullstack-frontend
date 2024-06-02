fcdd4fefa924a0b3aaf0f8547688f2c9f4216996
Amanda Ruiz <98482837+amilpsp@users.noreply.github.com>
2024-06-02 13:04:43 +0200

    if user in local storage is also in database, session (token) is kept and local storage user is set as user without authenticating.

--- src/contexts/AuthContext.tsx
+++ src/contexts/AuthContext.tsx
@@ -1,83 +1,118 @@
 import axios from 'axios';
-import React, { createContext, useState, ReactNode } from 'react';
+import React, { createContext, useState, ReactNode, useEffect } from "react";
 
 // Define types for user and context
+export interface Token {
+	user_id?: number;
+	username?: string;
+	token: string | null;
+}
 
 export interface AuthContextType {
-  user: User | null;
-  signup: (userData: User) => void;
-  login: (userData: User) => void;
-  logout: () => void;
+	user: User | null;
+	signup: (userData: User) => void;
+	login: (userData: User) => void;
+	logout: () => void;
+	checkUserSession: () => void;
 }
 
 // Create the context
 export const AuthContext = createContext<AuthContextType | undefined>(
-  undefined
+	undefined
 );
 
 interface AuthProviderProps {
-  children: ReactNode;
+	children: ReactNode;
 }
 
 // Create a provider component
 
 export const AuthProvider: React.FC<AuthProviderProps> = ({ children }) => {
-  const [user, setUser] = useState<User | null>(null);
+	const [user, setUser] = useState<User | null>(null);
 
-  const login = async (userData: User) => {
-    const loginConfig = {
-      method: 'post',
-      url: 'http://localhost:8080/login',
-      data: userData
-    };
+	const checkUserSession = async (/* userData: User */) => {
+		const storedUser = localStorage.getItem("userLogged");
 
-    try {
-      const result = await axios(loginConfig);
-      setUser(userData);
-      localStorage.setItem('userLogged', JSON.stringify(result.data));
-      console.log('Logged in user:', userData.username); // Log user data
-    } catch (error) {
-      throw new Error('Login failed');
-    }
-  };
+		const checkStoredConfig = {
+			method: "get",
+			url: "http://localhost:8080/login",
+			data: storedUser,
+		};
+		if (storedUser) {
+			try {
+				await axios(checkStoredConfig);
+				setUser(JSON.parse(storedUser));
+			} catch (error) {
+				console.error(
+					"Error verifying session, your session has been terminated, please log in again",
+					error
+				);
+				localStorage.removeItem("userLogged");
+			}
+		}
+	};
 
-  const signup = async (userData: User) => {
-    const signupConfig = {
-      method: 'post',
-      url: 'http://localhost:8080/signup',
-      data: userData
-    };
+	const login = async (userData: User) => {
+		const loginConfig = {
+			method: "post",
+			url: "http://localhost:8080/login",
+			data: userData,
+		};
 
-    try {
-      const result = await axios(signupConfig);
-      await login(result.data);
-    } catch (error) {
-      throw new Error('Signup failed');
-    }
-  };
+		try {
+			const result = await axios(loginConfig);
+			setUser(userData);
+			localStorage.setItem("userLogged", JSON.stringify(result.data));
+			/* 			console.log("Logged in user:", userData.username); // Log user data
+			 */
+		} catch (error) {
+			throw new Error("Login failed");
+		}
+	};
 
-  const logout = async () => {
-    const logoutData = JSON.parse(localStorage.getItem('userLogged') || '{}');
-    const logoutConfig = {
-      method: 'delete',
-      url: 'http://localhost:8080/login',
-      data: logoutData
-    };
+	const signup = async (userData: User) => {
+		const signupConfig = {
+			method: "post",
+			url: "http://localhost:8080/signup",
+			data: userData,
+		};
 
-    try {
-      await axios(logoutConfig);
-    } catch (error) {
-      console.log(
-        "wasn't able to remove the logged in session from the database"
-      );
-    }
-    localStorage.removeItem('userLogged');
-    setUser(null);
-  };
+		try {
+			const result = await axios(signupConfig);
+			await login(result.data);
+		} catch (error) {
+			throw new Error("Signup failed");
+		}
+	};
 
-  return (
-    <AuthContext.Provider value={{ user, signup, login, logout }}>
-      {children}
-    </AuthContext.Provider>
-  );
+	const logout = async () => {
+		const logoutData = JSON.parse(localStorage.getItem("userLogged") || "{}");
+		const logoutConfig = {
+			method: "delete",
+			url: "http://localhost:8080/login",
+			data: logoutData,
+		};
+
+		try {
+			await axios(logoutConfig);
+		} catch (error) {
+			console.log(
+				"wasn't able to remove the logged in session from the database"
+			);
+		}
+		localStorage.removeItem("userLogged");
+		setUser(null);
+	};
+
+	useEffect(() => {
+		checkUserSession();
+	}, []);
+
+	return (
+		<AuthContext.Provider
+			value={{ user, signup, login, logout, checkUserSession }}
+		>
+			{children}
+		</AuthContext.Provider>
+	);
 };
