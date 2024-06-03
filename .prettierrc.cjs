99e5916f4ad3a929eca3c0932a5527c2ddff310c
Andrei Pirogov Huertas <andrei.pirogov.huertas@gmail.com>
2024-06-03 02:48:59 +0200

    imported prettier and plugins with legacy deps

--- .prettierrc.cjs
+++ .prettierrc.cjs
@@ -1,5 +1,5 @@
 const config = {
-  trailingComma: "es5",
+  trailingComma: 'es5',
   tabWidth: 2,
   semi: true,
   singleQuote: true,
--- eslint.config.js
+++ eslint.config.js
@@ -3,16 +3,20 @@ import pluginJs from '@eslint/js';
 import tseslint, { plugin } from 'typescript-eslint';
 import pluginReactConfig from 'eslint-plugin-react/configs/recommended.js';
 import { fixupConfigRules } from '@eslint/compat';
+import prettier from 'prettier';
+import prettierConfig from 'eslint-config-prettier';
+import prettierPlugin from 'eslint-plugin-prettier';
 
 export default [
   { languageOptions: { globals: globals.browser } },
   pluginJs.configs.recommended,
   ...tseslint.configs.recommended,
   ...fixupConfigRules(pluginReactConfig),
-
+  prettierConfig,
   {
     rules: {
       'no-console': 'warn', // Add custom rules here
+      'prettier/prettier': 'error',
       semi: ['error', 'always'],
       quotes: ['error', 'single'],
       indent: ['error', 2],
